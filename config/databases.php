add0052b522284fd1a89478990151dcae4be87a2
herickbrandao <64874187+herickbrandao@users.noreply.github.com>
2021-01-01 23:10:39 -0300

    Update databases.php

--- config/databases.php
+++ config/databases.php
@@ -1,10 +1,10 @@
 <?php use \system\Database;
 
 Database::set(name: "my_db", data: [
-	'MYSQL',			# DB TYPE  / ex: MYSQL or PGSQL
-	'localhost',		# ADDRESS  / ex: localhost
-	'database_name',	# DB NAME  / ex: database_name
-	'root',				# DB USER  / ex: root
-	'',					# DB PASS  / ex: password
-	'',					# DB PORT  / ex: 5432
+	'MYSQL',            # DB TYPE  / ex: MYSQL or PGSQL
+	'localhost',        # ADDRESS  / ex: localhost
+	'database_name',    # DB NAME  / ex: database_name
+	'root',             # DB USER  / ex: root
+	'',                 # DB PASS  / ex: password
+	'',                 # DB PORT  / ex: 5432
 ]);
